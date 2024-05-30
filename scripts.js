7af9f129aa4b5f66175d5f1f746ebbad3c79b18f
PGtheVRguy <46075828+PGtheVRguy@users.noreply.github.com>
2024-05-30 19:48:06 -0400

    Added navbar :3

--- scripts.js
+++ scripts.js
@@ -1,17 +1,3 @@
-/**
- * Returns a number whose value is limited to the given range.
- *
- * Example: limit the output of this computation to between 0 and 255
- * (x * 255).clamp(0, 255)
- *
- * @param {Number} min The lower boundary of the output range
- * @param {Number} max The upper boundary of the output range
- * @returns A number in the range [min, max]
- * @type Number
- */
-Number.prototype.clamp = function(min, max) {
-    return Math.min(Math.max(this, min), max);
-  };
 
 
 document.addEventListener('DOMContentLoaded', function() {
@@ -114,3 +100,15 @@ document.addEventListener('DOMContentLoaded', function() {
         el.addEventListener('mouseleave', () => el.classList.remove('tilt'));
     });
 });
+
+
+const navbar = document.querySelector('#NavBar');
+let navtop = navbar.offsetTop;
+function stickynavbar() {
+  if (window.scrollY >= navtop) {    
+    navbar.classList.add('sticky');
+  } else {
+    navbar.classList.remove('sticky');    
+  }
+}
+window.addEventListener('scroll', stickynavbar);
